a2d070af3a7bb82a344f2c8e70f9b28016e419b3
RaffaelSchaefer <raffael.elias.schaefer@gmail.com>
2020-08-11 21:16:54 +0200

    Update

--- lib/Widget/cards.dart
+++ lib/Widget/cards.dart
@@ -10,15 +10,20 @@ class SimpleCard extends StatelessWidget {
           padding: EdgeInsets.all(5),
           child: Column(
             children: [
-              Text(
-                "Lore Ipsum",
-                style: TextStyle(
-                  fontSize: 40.0,
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Lore Ipsum",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                  ),
                 ),
               ),
               Divider(),
               Text(
                 "dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet",
+                textAlign: TextAlign.left,
                 style: TextStyle(fontSize: 15, color: Colors.grey.shade600),
               )
             ],
